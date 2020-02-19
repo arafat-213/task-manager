1e0b2217c4df84363e5235ea10ef383ac121e4fc
Arafat Tai <tai.arafat.at@gmail.com>
2020-02-19 21:50:43 +0530

    Added query code

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const mongodb = require('mongodb')
-
 // MongoClient for accessing the database 
-const MongoClient = mongodb.MongoClient
+const { MongoClient } = require('mongodb')
 
 // Connection URL for the database, 'localhost' is malfunctioning hence IP of localhost
 const connectionURL = 'mongodb://127.0.0.1:27017'
@@ -20,16 +18,49 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     const database = client.db(databaseName)
 
     // Creates/Reads the collection named 'users' and inserts the passed values as document(s)
-    database.collection('users').insertMany([{
-        name: 'Tony',
-        Age: 46
-    }, {
-        name: 'Steve',
-        Age: 91
-    }], (error, result) => {
-        if(error) {
-            return console.log('Unable to add user to the database');
-        }
-        console.log(result.ops);
-    })
+    // database.collection('users').insertMany([{
+    //     name: 'Tony',
+    //     Age: 46
+    // }, {
+    //     name: 'Steve',
+    //     Age: 91
+    // }], (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to add user to the database');
+    //     }
+    //     console.log(result.ops);
+    // })
+
+    // database.collection('kaaam').insertMany([
+    //     {
+    //         description: 'Shopping',
+    //         completed: true
+    //     },
+    //     {
+    //         description: 'Cleaning',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'Sleeping',
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return  console.log('Unable to add kaams');
+    //     }
+    //     console.log(result.ops);
+        
+    // })
+
+    // Finds the first kaam which is not completed
+    // database.collection('kaaam').findOne({completed: false}, (error, kaaam)=> {
+        // if(!error)
+        //    console.log(kaaam);
+            
+    // })
+
+    // Finds all the kaams that are not completed
+    // database.collection('kaaam').find({completed: false}).toArray((error, kaaam) => {
+    //     console.log(kaaam);
+    // })
 })
